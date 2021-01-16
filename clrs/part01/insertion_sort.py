c3367db68b35584f742001723619c5876fa9b412
phamvanthanh <phamvanthanh6093@gmail.com>
2021-01-16 18:59:15 +0700

    Insertion sort in python

--- clrs/part01/insertion_sort.py
+++ clrs/part01/insertion_sort.py
@@ -9,6 +9,18 @@ def insertionsort(a):
 
 	return a
 
+def insertionsort_dec(a):
+	for i in range(1, len(a)):
+		key = a[i]
+		j = i - 1
+		while j >= 0 and a[j] < key:
+			a[j+1] = a[j]
+			j -= 1
+		a[j+1] = key
+
+	return a
+
 if __name__ == "__main__":
 	a = [5, 2, 4, 6, 1, 3]
-	print (insertionsort(a))
+	# print (insertionsort(a))
+	print (insertionsort_dec(a))
